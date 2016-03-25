a25adda4a5f15ee0bc15a57c8382ab04972b1a75
lyw0149 <lyw0149@gmail.com>
2016-03-25 13:21:21 +0900

    Added files via upload

---
+++ Example6_BT_Serial_command/BT_Serial_command.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2, 3); //Connect HC-06 TX,RX
+
+void setup()  
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+
+  // set the data rate for the BT port
+  BTSerial.begin(9600);
+}
+
+void loop()
+{
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+}
+
